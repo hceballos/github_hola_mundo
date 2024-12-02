56d2f014d9874d6a714dc8a78a1c0e27bb5d9744
Hector Ceballos Avilés <35620898+hceballos@users.noreply.github.com>
2024-12-02 12:47:02 -0300

    Update fin.py

--- fin.py
+++ fin.py
@@ -1,6 +1,6 @@
 print("fin del proceso - editado version 2, lunes 02 12:37")
 x = 5
 while x > 0:
-  x -= 1
-  print(x)
-  print('¡Pum!')
+    x -= 1  # Decrementar x primero
+    print(x)  # Imprimir el valor de x después de restarle
+    print('¡Pum!')  # Imprimir '¡Pum!' después
