8f59073d3c9342f34cf632f7e1c2198a6a6d22e2
Hector Ceballos Avilés <35620898+hceballos@users.noreply.github.com>
2024-12-05 11:40:23 -0300

    Update github_hola_mundo.py

--- github_hola_mundo.py
+++ github_hola_mundo.py
@@ -1,2 +1,9 @@
 print("hola mundo")
 print(f"Hola, {{ usuario }}!")
+
+devengo = pd.DataFrame()
+for f in glob.glob('/Users/hector/Downloads/80 BIS.xlsx'): # "../mejorninez/input_excel/pagoManual/*",
+	df = pd.read_excel(f, converters={ 'folio': str, 'Nº CDP': str, 'Monto Total': int } )
+	print('Procesando  : ', f)
+	devengo = pd.concat([devengo, df], ignore_index=True)
+print(devengo)
